0fe9915045620eef888624b3cdf3cb28dd9b510d
crisp-panel-7711 <secharmot@davidson.edu>
2019-01-18 21:45:23 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(arg1 = "Squawk!")
+def parrot(phrase = "Squawk!")
   print "#{arg1}"
   puts "#{arg1}"
 end
